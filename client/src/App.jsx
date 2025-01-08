5e0de57348083471c8ece83657e4d2a3d0baff61
Maria Nandawula <110343215+triscillanandy@users.noreply.github.com>
2025-01-08 20:16:25 +0300

    Add i

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,8 +19,8 @@ import LandingPage1 from "./Sections/LandingPage1";
 //import Reels from "./Sections/Component/Reels";
 // import Posts from "./Sections/Component/Posts";
 import EditProfile from "./Sections/Component/EditProfile";
-import Denomination from "./Sections/Component/Denomination";
-import Chat from "./Sections/Component/Chat";
+// import Denomination from "./Sections/Component/Denomination";
+// import Chat from "./Sections/Component/Chat";
 // import { MyProvider } from "./GlobalVariables";
 // import Plans from "../../Sections/Plans.jsx";
 // import Plans from "./Sections/Plans";
@@ -69,10 +69,10 @@ function App() {
           {/* <Route path="/reels" element={<Reels></Reels>} /> */}
           {/* <Route path="/post" element={<Posts></Posts>} /> */}
           <Route path="/edit-profile" element={<EditProfile />} />
-          <Route path="/denomination" element={<Denomination></Denomination>} />
+          {/* <Route path="/denomination" element={<Denomination></Denomination>} /> */}
           {/* <Route path = "/devotional" element = {} /> */}
           {/* <Route path /> */}
-          <Route path="/chats" element={<Chat></Chat>} />
+          {/* <Route path="/chats" element={<Chat></Chat>} /> */}
         </Routes>
       </Router>
     </>
